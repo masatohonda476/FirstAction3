0b571892cc5e0fbcf2e6cc8467c79c5ed7e3eddc
本多優仁 <masato0116honda@gmail.com>
2026-05-14 14:33:25 +0900

    SOの情報の追加でステータス画面にアイテム数を出せるよう変更
    - SOの情報追加でアイテム数を表示できるよう変更

--- Assets/Scripts/ItemBoxManager.cs
+++ Assets/Scripts/ItemBoxManager.cs
@@ -2,15 +2,18 @@ using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using System.IO.Compression;
 
 public class ItemBoxManager : MonoBehaviour
 {
     [SerializeField] ItemsSO itemsSO;
-    [SerializeField] TextMeshProUGUI coinText;
-    [SerializeField] TextMeshProUGUI potionText;
+    [SerializeField] TextMeshProUGUI itemText;
+    //[SerializeField] TextMeshProUGUI coinText;
+    //[SerializeField] TextMeshProUGUI potionText;
     public int getItem;
     
     private int[] itemQtyAry; //各アイテムにの所持数を格納する配列
+    private string itemOpenText; //
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -32,7 +35,16 @@ public class ItemBoxManager : MonoBehaviour
 
     public void ItemOpen()
     {
-        coinText.GetComponent<TextMeshProUGUI>().text = "Coin: " + itemQtyAry[0].ToString();
-        potionText.GetComponent<TextMeshProUGUI>().text = "Potion: " + itemQtyAry[1].ToString();
+        //coinText.GetComponent<TextMeshProUGUI>().text = "Coin: " + itemQtyAry[0].ToString();
+        //potionText.GetComponent<TextMeshProUGUI>().text = "Potion: " + itemQtyAry[1].ToString();
+        for (int i = 0; i < itemQtyAry.Length; i++)
+        {
+            if (itemQtyAry[i] > 0)
+            {
+                itemOpenText = itemOpenText + itemsSO.itemList[i].ItemName + " " + itemQtyAry[i].ToString() + "\n";
+                itemText.GetComponent<TextMeshProUGUI>().text = itemOpenText;
+            }
+        }
+        itemOpenText = ""; //
     }
 }
